77c6042bda2bb1d3e7525fc6a2d8b5062969bdbd
misostack <misostack.com@gmail.com>
2019-12-24 14:35:28 +0700

    Update votes with slices

--- game11/game11.go
+++ game11/game11.go
@@ -8,18 +8,6 @@ import (
 	"bufio"
 )
 
-var names [10]string = [10]string{ 
-	"Dat Xanh Group", 
-	"FLC Group", 
-	"Vingroup", 
-	"CapitaLand",
-	"Hung Thinh Real Estate Business Investment Corporation",
-	"Novaland",
-	"Phat Dat Real Estate Development Corporation",
-	"Phu My Hung",
-	"Nam Long Investment Corporation",
-	"Sun Group",
-}
 
 func main() {
 	/*
@@ -32,8 +20,9 @@ func main() {
 	// status, err := generateData(filePath)
 	// if err != nil { fmt.Println(err) }
 	// if status { fmt.Println("Otoke") }
-	var results []int	
-	countVotes(filePath, &results)
+	results := []int{}
+	names := []string{}
+	countVotes(filePath, &names, &results)
 	printVotesResult(names, results)
 }
 
@@ -44,33 +33,46 @@ func printArray(arr []int) {
 	fmt.Printf("\n")
 }
 
-func printVotesResult(names [10]string, results []int) {
+func printVotesResult(names []string, results []int) {
 	for index := 0; index < len(names); index++ {
-		fmt.Printf("%v : %v votes \n", names[index], results[index])
+		fmt.Printf("%v. %v : %v votes \n", index + 1, names[index], results[index])
 	}
 }
 
-func countVotes(filePath string, results *[]int) error{
-	// https://www.geeksforgeeks.org/golang-pointer-to-an-array-as-function-argument/
-	
-	*results = make([]int, 10)
-	// reset votes to 0 for all
-	for i := 0; i < len(*results); i++ {
-		(*results)[i] = 0
-	}
+func readLines(filePath string) ([]string, error) {
+	lines := []string{} // empty slice
 	fh, err := os.OpenFile(filePath, os.O_RDONLY, 0700)
 	if err != nil {
 		fh.Close()
-		return err
+		return lines, err
 	}
 	scanner := bufio.NewScanner(fh)
 	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fh.Close()
+	return lines, nil
+}
+
+func countVotes(filePath string, names *[]string, counts *[]int) error{
+	// https://www.geeksforgeeks.org/golang-pointer-to-an-array-as-function-argument/
+
+	var matched bool
+	lines, err := readLines(filePath)
+	if err != nil { return err }
+	for _, line := range lines {
 		// count votes
-		for idx, name := range names {
-			if scanner.Text() == name {
-				(*results)[idx] ++
+		matched = false
+		for idx, name := range (*names) {
+			if line == name {
+				matched = true
+				(*counts)[idx]++
 				break
 			}
+		}
+		if !matched {			
+			*names = append(*names, line)
+			*counts = append(*counts, 1)
 		}
 	}
 	return nil
