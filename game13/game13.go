4e2a77778ae51edad5cc4d1553e8f4bf5f12ab2b
misostack <misostack.com@gmail.com>
2019-12-26 17:52:45 +0700

    Headfirst series game 13, the challenger

---
+++ game13/game13.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("[Headfirst Series] Game 13 : The Challenger, read the data and transform it to json file")
+	// data structure
+	// xx	ABC DEF XYZ (relatives)	DD/MM/YYYY
+	// Todo
+	// - Read the file
+	// - Split the data in a line into a struct {id, name, isRelatives?, dob}
+	// - Save new data into a new file with json format
+}
