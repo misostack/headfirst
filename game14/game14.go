fd11238e663ecade0107b0ac3dba1cdb6775b016
misostack <misostack.com@gmail.com>
2019-12-30 18:28:49 +0700

    Defined Types

---
+++ game14/game14.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+)
+// MyType is a type of string
+type MyType string
+
+// CMTOPIXEL : 1cm = 37.7952755906 px
+const CMTOPIXEL = 37.7952755906
+
+// PTTOPIXEL : 1pt = 37.7952755906 px
+const PTTOPIXEL = 1.333333
+
+// Centimeter : cm
+type Centimeter float64
+// Pixel : px
+type Pixel float64
+// Point : pt
+type Point float64
+
+// ToPixel : convert centimeter to pixel
+func (c Centimeter) ToPixel() Pixel {
+	return Pixel( c * CMTOPIXEL)
+}
+
+// ToPixel : convert PT to Pixel
+func (p Point) ToPixel() Pixel {
+	return Pixel( p * PTTOPIXEL)
+}
+
+
+func (m MyType) firstLetter() string{
+	return string(m[0])
+}
+
+func (m *MyType) capitalize() {
+	cm := map[rune]rune{}
+	uc := 'A'
+	for lc := 'a'; lc<= 'z'; lc++{		
+		cm[lc] = uc
+		uc++
+	}
+
+	// uc := map[byte]byte{
+	ns := []rune{}
+	// }
+	for i,v := range *m {
+		if i == 0 {
+			c, ok := cm[v]
+			if ok {
+				ns = append(ns, c)
+			}		
+		} else {
+			ns = append(ns, v)
+		}
+	}
+	*m = ""
+	for _, v := range ns {
+		*m += MyType(v)
+	}
+}
+
+func main() {
+	fmt.Println("[Headfirst Series]: Game 14 , the secret of Defined Types.")
+	value := MyType("the first letter should be capitalized")
+	fmt.Println(value)
+	fmt.Printf("The first letter of %v is '%v'\n", value, value.firstLetter())
+	value.capitalize()
+	fmt.Println(value)
+	cx, cy := Centimeter(2.5), Centimeter(3)
+	fmt.Printf("cx=%v and cy=%v ==> x=%v and y=%v\n", cx, cy, cx.ToPixel(), cy.ToPixel())
+	px, py := Point(16), Point(20)
+	fmt.Printf("px=%v and py=%v ==> x=%v and y=%v\n", px, py, px.ToPixel(), py.ToPixel())
+}
